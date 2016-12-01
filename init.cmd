a80f0706de8f403b53908a3aad42a6d6b204bc59
Rustem Nasibullin <rnasibullin@mtt.ru>
2016-12-01 15:40:43 +0300

    a

--- init.cmd
+++
@@ -1,6 +0,0 @@
-echo "# scheduler" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/rustemnasibullin/scheduler.git
-git push -u origin master
